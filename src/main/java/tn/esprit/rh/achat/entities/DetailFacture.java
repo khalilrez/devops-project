8f0727acf77d48243d64331d723236d8306c53eb
Rmayssa2 <mayssa.riahi@esprit.tn>
2023-10-06 19:27:27 +0100

    first commit by mayssa

--- src/main/java/tn/esprit/rh/achat/entities/DetailFacture.java
+++ src/main/java/tn/esprit/rh/achat/entities/DetailFacture.java
@@ -21,6 +21,7 @@ public class DetailFacture implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//mayssa
 	private Long idDetailFacture;
 	private Integer qteCommandee;
 	private float prixTotalDetail;
